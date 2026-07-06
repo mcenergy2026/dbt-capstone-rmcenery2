4bdebc20995808786329a499107e892b7b539990
Richard McEnery <rmcenery@phdata.io>
2026-07-06 15:54:07 +0000

    checked tests

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -8,32 +8,14 @@ WITH int_order_details AS (
 final AS (
 
     SELECT
-        order_number,
+        order_number AS order_pk,
+        product_code AS product_pk,
+        customer_number AS customer_pk,
         order_line_number,
         product_code,
-        order_date,
-        required_date,
-        shipped_date,
-        status,
-        comments,
-        customer_number,
-        customer_name,
-        customer_first_name,
-        customer_last_name,
-        customer_phone,
-        customer_city,
-        customer_state,
-        customer_postal_code,
-        customer_country,
-        sales_rep_employee_number,
-        product_name,
-        product_line,
-        product_scale,
-        product_vendor,
         quantity_ordered,
         price_each,
-        line_amount,
-        _sync_date
+        line_amount
     FROM int_order_details
 
 )
